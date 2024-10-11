3c80e340e06b567bb6142106f227396cc86eed99
Luke Watson <lukewatsontoronto@gmail.com>
2024-10-11 01:20:23 -0400

    made some more progress

--- telemetry_source/include/global.h
+++ telemetry_source/include/global.h
@@ -1,11 +1,16 @@
 #ifndef SET_VALS_H
 #define SET_VALS_H
 
-#define RX_PIN 0
-#define TX_PIN 1
 #define XBEE_BAUD_RATE 57600
 #define CMD_BUFF_SIZE 100
 #define CMD_WORD_SIZE 50
+
+enum mode_control {
+    FLIGHT = 0,
+    SIM = 1,
+    STANDBY = 2,
+    SIM_READY = 3
+};
 
 struct command_packet {
     char[CMD_WORD_SIZE] keyword;
--- telemetry_source/src/main.cpp
+++ telemetry_source/src/main.cpp
@@ -3,6 +3,12 @@
 #include "global.h"
 #include "helpers.h"
 
+/*------- DEFINITIONS ------*/
+#define RX_PIN 0
+#define TX_PIN 1
+/*------- DEFINITIONS ------*/
+
+/*------- FUNCTIONS ------*/
 void IRAM_ATTR send_1();
 void do_cx(char *data);
 void do_st(char *data);
@@ -11,16 +17,20 @@ void do_simp(char *data);
 void do_cal(char *data);
 void do_mec(char *data);
 void do_reset_team_id(char *data);
+/*------- FUNCTIONS ------*/
 
+/*------- VARIABLES ------*/
 hw_timer_t *send_timer = NULL;
 Packet *pckt = new Packet();
 char cmd_buff[CMD_BUFF_SIZE];
 int TEAM_ID = 9999;
+mode_control current_mode = STANDBY;
+/*------- VARIABLES ------*/
 
 void setup() 
 {
 
-  // Set up 1Hz timer
+  // Set timer to transmit at 1Hz
   send_timer = timerBegin(0, 80, true);
   timerAttachInterrupt(send_timer, &send_1, true);
   timerAlarmWrite(send_timer, 1000000, true);
@@ -32,7 +42,7 @@ void setup()
   Serial2.begin(XBEE_BAUD_RATE, SERIAL_8N1, RX_PIN, TX_PIN);
   delay(1000);
 
-  Serial.println("SETUP OK");
+  Serial.println("$I SETUP OK");
 }
 
 void loop() 
@@ -48,7 +58,7 @@ void loop()
 
       if(bytes_read == CMD_BUFF_SIZE - 1)
       {
-        Serial2.println("COMMAND REJECTED: LONGER THAN ALLOCATED BUFFER SIZE\n");
+        Serial2.println("$W COMMAND REJECTED: LONGER THAN ALLOCATED BUFFER SIZE\n");
         break;
       }
 
@@ -56,19 +66,19 @@ void loop()
       struct command_packet recv_packet;
       if(extract_cmd_msg(cmd_buff, &recv_packet) > 0)
       {
-        Serial2.println("COMMAND REJECTED: FORMAT IS INCORRECT. EXPECTING 'CMD,<TEAM_ID>,<CMD>,<DATA>'\n");
+        Serial2.println("$W COMMAND REJECTED: FORMAT IS INCORRECT. EXPECTING 'CMD,<TEAM_ID>,<CMD>,<DATA>'\n");
         break;
       }
       if(comapre_strings(recv_packet.keyword, "CMD") > 0)
       {
-        Serial2.println("REJECTED INPUT: NOT CMD\n");
+        Serial2.println("$W REJECTED INPUT: NOT CMD\n");
         break;
       }
       int team_id_chk_int;
       sscanf(recv_packet.team_id, "%d", &team_id_chk_int);
       if(team_id_chk_int != TEAM_ID && compare_strings(recv_packet.command, "RESET_TEAM_ID"))
       {
-        Serial2.println("REJECTED INPUT: TEAM ID DOES NOT MATCH.\n");
+        Serial2.println("$W REJECTED INPUT: TEAM ID DOES NOT MATCH.\n");
         break;
       }
 
@@ -83,7 +93,7 @@ void loop()
       else if(compare_strings(msg, "RESET_TEAM_ID") == 0) do_reset_team_id(recv_packet.data);
       else
       {
-        Serial2.println("COMMAND REJECTED: NOT A COMMAND\n");
+        Serial2.println("$W COMMAND REJECTED: NOT A COMMAND\n");
       }
 
     }
@@ -94,27 +104,82 @@ void loop()
 void do_cx(char *data)
 {
   // TODO: Multi-thread for data sending and command reception
-  Serial2.println("RECEIVED: CX\n");
+  // TODO: If CX OFF received set mode to STANDBY
+  Serial2.println("$I RECEIVED COMMAND: BEGIN PAYLOAD TRANSMISSION\n");
 }
 
 void do_st(char *data)
 {
-  Serial2.println("RECEIVED: ST\n");
+  Serial2.println("$I RECEIVED COMMAND: SET TIME\n");
+  // TODO: What do they mean by this??
+  // <UTC_TIME>|GPS is UTC time in the format hh:mm:ss or ‘GPS’ which sets the
+  // flight software time to the current time read from the GPS module
 }
 
 void do_simp(char *data)
 {
-  Serial2.println("RECEIVED: SIMP\n");
+  Serial2.println("$I RECEIVED COMMAND: SIMULATION MODE CONTROL\n");
+  if(compare_strings(data, "ENABLE"))
+  {
+    switch(current_mode)
+    {
+      case STANDBY:
+                  current_mode = SIM_READY;
+                  Serial2.println("$I MODE SET: SIM_READY\n");
+                  break;
+      case SIM_READY:
+                  Serial2.println("$I MODE IS ALREADY SET TO: SIM_READY\n");
+                  break;
+      default:
+                  Serial2.println("$E PAYLOAD IS NOT IN STANDBY MODE\n");
+    }
+  }
+  else if(compare_strings(data, "ACTIVATE"))
+  {
+    switch(current_mode)
+    {
+      case SIM_READY:
+                  Serial2.println("$I STARTING SIMULATION MODE...\n");
+                  // TODO
+                  break;
+      case STANDBY:
+                  Serial2.println("$E SIMULATION MODE IS NOT ENABLED\n");
+                  break;
+      default:
+                  Serial2.println("$E PAYLOAD IS NOT IN STANDBY MODE\n");
+    }
+  }
+  else if(compare_string(data, "DISABLE"))
+  {
+    switch(current_mode)
+    {
+      case SIM:
+                  Serial2.println("$I ENDING SIMULATION MODE...\n");
+                  // TODO
+                  break;
+      case FLIGHT:
+                  Serial2.println("$E PAYLOAD IS IN FLIGHT MODE\n");
+                  break;
+      default:
+                  Serial2.println("$E PAYLOAD IS READY/ON STANDBY\n");
+    }
+  }
+  else
+  {
+    Serial2.println("$E UNRECOGNIZED SIM COMMAND: '" + String(data) + "'\n");
+  }
 }
 
 void do_cal(char *data)
 {
-  Serial2.println("RECEIVED: CAL\n");
+  Serial2.println("$I RECEIVED COMMAND: CAL\n");
+  // TODO: Set up watchdog timer/critical state flash memory to recover from hanging/unexpected reset
 }
 
 void do_mec(char *data)
 {
-  Serial2.println("RECEIVED: MEC\n");
+  Serial2.println("$I RECEIVED COMMAND: MEC\n");
+  // TODO: complete after sensore are installed
 }
 
 void do_reset_team_id(char *data)
@@ -127,7 +192,5 @@ void do_reset_team_id(char *data)
 
 void IRAM_ATTR send_1()
 {
-  String send_msg;
-  create_msg(pckt, send_msg);
-  Serial.println("THIS WILL BE A PACKET");
+  // TODO: This will send a packet regardless if it is ready
 }
