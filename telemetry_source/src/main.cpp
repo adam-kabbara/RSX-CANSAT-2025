6b994b971007182a77c99dff6cb4af24b7998df0
Luke Watson <lukewatsontoronto@gmail.com>
2024-10-12 02:52:03 -0400

    fixes

--- telemetry_source/src/main.cpp
+++ telemetry_source/src/main.cpp
@@ -1,27 +1,25 @@
 #include <Arduino.h>
-#include "packet.h"
 #include "global.h"
 #include "helpers.h"
 
 /*------- DEFINITIONS ------*/
-#define RX_PIN 0
-#define TX_PIN 1
+#define RX_PIN 16
+#define TX_PIN 17
 /*------- DEFINITIONS ------*/
 
 /*------- FUNCTIONS ------*/
 void IRAM_ATTR send_1();
-void do_cx(char *data);
-void do_st(char *data);
-void do_sim(char *data);
-void do_simp(char *data);
-void do_cal(char *data);
-void do_mec(char *data);
-void do_reset_team_id(char *data);
+void do_cx(const char *data);
+void do_st(const char *data);
+void do_sim(const char *data);
+void do_simp(const char *data);
+void do_cal(const char *data);
+void do_mec(const char *data);
+void do_reset_team_id(const char *data);
 /*------- FUNCTIONS ------*/
 
 /*------- VARIABLES ------*/
 hw_timer_t *send_timer = NULL;
-Packet *pckt = new Packet();
 char cmd_buff[CMD_BUFF_SIZE];
 int TEAM_ID = 9999;
 mode_control current_mode = STANDBY;
@@ -42,7 +40,7 @@ void setup()
   Serial2.begin(XBEE_BAUD_RATE, SERIAL_8N1, RX_PIN, TX_PIN);
   delay(1000);
 
-  Serial.println("$I SETUP OK");
+  Serial2.println("\n$I SETUP OK\n");
 }
 
 void loop() 
@@ -53,6 +51,7 @@ void loop()
   {
     if(Serial2.available())
     {
+      memset(cmd_buff, 0, CMD_BUFF_SIZE);
       byte bytes_read = Serial2.readBytesUntil('\n', cmd_buff, CMD_BUFF_SIZE - 1);
       cmd_buff[bytes_read] = '\0';
 
@@ -69,7 +68,7 @@ void loop()
         Serial2.println("$W COMMAND REJECTED: FORMAT IS INCORRECT. EXPECTING 'CMD,<TEAM_ID>,<CMD>,<DATA>'\n");
         break;
       }
-      if(comapre_strings(recv_packet.keyword, "CMD") > 0)
+      if(compare_strings(recv_packet.keyword, "CMD") > 0)
       {
         Serial2.println("$W REJECTED INPUT: NOT CMD\n");
         break;
@@ -83,14 +82,13 @@ void loop()
       }
 
       // Parse command
-      // TODO: Is using function pointer array faster? If else is messy but speed might be better
-      if(compare_strings(msg, "CX") == 0) do_cx(recv_packet.data);
-      else if(compare_strings(msg, "ST") == 0) do_st(recv_packet.data);
-      else if(compare_strings(msg, "SIM") == 0) do_sim(recv_packet.data);
-      else if(compare_strings(msg, "SIMP") == 0) do_simp(recv_packet.data);
-      else if(compare_strings(msg, "CAL") == 0) do_cal(recv_packet.data);
-      else if(compare_strings(msg, "MEC") == 0) do_mec(recv_packet.data);
-      else if(compare_strings(msg, "RESET_TEAM_ID") == 0) do_reset_team_id(recv_packet.data);
+      if(compare_strings(recv_packet.command, "CX") == 0) do_cx(recv_packet.data);
+      else if(compare_strings(recv_packet.command, "ST") == 0) do_st(recv_packet.data);
+      else if(compare_strings(recv_packet.command, "SIM") == 0) do_sim(recv_packet.data);
+      else if(compare_strings(recv_packet.command, "SIMP") == 0) do_simp(recv_packet.data);
+      else if(compare_strings(recv_packet.command, "CAL") == 0) do_cal(recv_packet.data);
+      else if(compare_strings(recv_packet.command, "MEC") == 0) do_mec(recv_packet.data);
+      else if(compare_strings(recv_packet.command, "RESET_TEAM_ID") == 0) do_reset_team_id(recv_packet.data);
       else
       {
         Serial2.println("$W COMMAND REJECTED: NOT A COMMAND\n");
@@ -101,14 +99,14 @@ void loop()
 
 }
 
-void do_cx(char *data)
+void do_cx(const char *data)
 {
   // TODO: Multi-thread for data sending and command reception
   // TODO: If CX OFF received set mode to STANDBY
   Serial2.println("$I RECEIVED COMMAND: BEGIN PAYLOAD TRANSMISSION\n");
 }
 
-void do_st(char *data)
+void do_st(const char *data)
 {
   Serial2.println("$I RECEIVED COMMAND: SET TIME\n");
   // TODO: What do they mean by this??
@@ -116,7 +114,7 @@ void do_st(char *data)
   // flight software time to the current time read from the GPS module
 }
 
-void do_simp(char *data)
+void do_sim(const char *data)
 {
   Serial2.println("$I RECEIVED COMMAND: SIMULATION MODE CONTROL\n");
   if(compare_strings(data, "ENABLE"))
@@ -149,7 +147,7 @@ void do_simp(char *data)
                   Serial2.println("$E PAYLOAD IS NOT IN STANDBY MODE\n");
     }
   }
-  else if(compare_string(data, "DISABLE"))
+  else if(compare_strings(data, "DISABLE"))
   {
     switch(current_mode)
     {
@@ -170,24 +168,29 @@ void do_simp(char *data)
   }
 }
 
-void do_cal(char *data)
+void do_simp(const char *data)
+{
+  Serial2.println("$I RECEIVED COMMAND: SIMP\n");
+}
+
+void do_cal(const char *data)
 {
   Serial2.println("$I RECEIVED COMMAND: CAL\n");
   // TODO: Set up watchdog timer/critical state flash memory to recover from hanging/unexpected reset
 }
 
-void do_mec(char *data)
+void do_mec(const char *data)
 {
   Serial2.println("$I RECEIVED COMMAND: MEC\n");
   // TODO: complete after sensore are installed
 }
 
-void do_reset_team_id(char *data)
+void do_reset_team_id(const char *data)
 {
   int new_team_id;
   sscanf(data, "%d", &new_team_id);
   TEAM_ID = new_team_id;
-  Serial2.println("TEAM ID HAS BEEN RESET TO %d\n", new_team_id);
+  Serial2.printf("TEAM ID HAS BEEN RESET TO %d\n", new_team_id);
 }
 
 void IRAM_ATTR send_1()
