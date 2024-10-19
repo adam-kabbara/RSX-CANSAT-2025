440d28c1268c0ed47b86267010828c4b755d8f3d
Daniel Yu <danielu6776@gmail.com>
2024-10-19 14:59:17 -0400

    Added rpm calculations

--- sensor_testing/ardunio_files/hall_sensor_testing/hall_sensor_testing.ino
+++ sensor_testing/ardunio_files/hall_sensor_testing/hall_sensor_testing.ino
@@ -2,9 +2,13 @@
 // CANSAT 2T4-2T5
 
 int hallSensorPin = 2;     
-int ledPin =  13;    
-int state = 0;
-int counter = 0;        
+int ledPin =  13;
+int currState = 0;
+int lastState = 1;
+int counter = 0; 
+float onTime = 0.0;
+float offTime = 0.0;
+float rpm = 0.0;
 
 void setup() {
   Serial.begin(115200);
@@ -18,21 +22,49 @@ void setup() {
 void loop(){
   counter++;
 
-  state = digitalRead(hallSensorPin);
+  currState = digitalRead(hallSensorPin);
 
-  if (state == LOW) { 
+  if (currState == LOW) {
     digitalWrite(ledPin, HIGH);
-    Serial.println("Detected Magnet: LED ON!");
-    Serial.print("At Time: ");
-    Serial.print(counter * 0.01);
-    Serial.println(" seconds.");
-    while (true) {
-      // DO NOTHING
+    if (lastState != currState) {
+      Serial.println(" ");
+      Serial.println("Detected Magnet: LED ON!");
+      Serial.print("At Time: ");
+      onTime = counter * 0.01;
+      Serial.print(onTime);
+      Serial.println(" seconds.");
     }
   } else {
     digitalWrite(ledPin, LOW);
-    Serial.println("Lost Magnet: LED OFF!");
+    if (lastState != currState) {
+      Serial.println(" ");
+      Serial.println("Lost Magnet: LED OFF!");
+      Serial.print("At Time: ");
+      offTime = counter * 0.01;
+      Serial.print(offTime);
+      Serial.println(" seconds.");
+    }
   }
 
+  calculateRPM(onTime, offTime);
+  Serial.print("Current RPM: ");
+  Serial.println(rpm);
+
+  lastState = currState;
   delay(10);
+}
+
+float calculateRPM(float start, float end) {
+  float oneRotationTime = end - start;
+  Serial.println(end);
+  Serial.println(start);
+  Serial.println(oneRotationTime);
+  
+  if (oneRotationTime > 0) {
+    rpm = (60.0 / oneRotationTime);
+  } else {
+    rpm = 0.0;
+  }
+
+  return rpm;
 }
