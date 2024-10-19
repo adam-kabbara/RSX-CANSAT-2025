a07318d8e008c18a72267c415f54b4302a7b2147
Daniel Yu <danielu6776@gmail.com>
2024-10-19 11:54:38 -0400

    Added a time calculating mechanism to detect when the magnet is first hit

--- sensor_testing/ardunio_files/hall_sensor_testing/hall_sensor_testing.ino
+++ sensor_testing/ardunio_files/hall_sensor_testing/hall_sensor_testing.ino
@@ -3,7 +3,8 @@
 
 int hallSensorPin = 2;     
 int ledPin =  13;    
-int state = 0;          
+int state = 0;
+int counter = 0;        
 
 void setup() {
   Serial.begin(115200);
@@ -15,13 +16,23 @@ void setup() {
 }
 
 void loop(){
+  counter++;
+
   state = digitalRead(hallSensorPin);
 
   if (state == LOW) { 
     digitalWrite(ledPin, HIGH);
     Serial.println("Detected Magnet: LED ON!");
+    Serial.print("At Time: ");
+    Serial.print(counter * 0.01);
+    Serial.println(" seconds.");
+    while (true) {
+      // DO NOTHING
+    }
   } else {
     digitalWrite(ledPin, LOW);
     Serial.println("Lost Magnet: LED OFF!");
   }
+
+  delay(10);
 }
