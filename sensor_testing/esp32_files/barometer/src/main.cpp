ffe45703641d35fd988bc78ae0cbdfd29f9a6f9d
Daniel Yu <danielu6776@gmail.com>
2024-11-02 01:43:14 -0400

    Cleaned up barometer code

--- sensor_testing/esp32_files/barometer/src/main.cpp
+++ sensor_testing/esp32_files/barometer/src/main.cpp
@@ -9,38 +9,31 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
+// Standard atmospheric pressure is 1013.25 hPa --> https://en.wikipedia.org/wiki/Atmospheric_pressure, https://www.noaa.gov/jetstream/atmosphere/air-pressure
 #define SEALEVELPRESSURE_HPA (1013.25)
 
 Adafruit_BME280 bme;
 
 void printValues();
 
-unsigned long delayTime;
-
 void setup() {
     Serial.begin(9600);
-    Serial.println(F("Beginnning BME280 Testing..."));
+    Serial.println(F("\nBooting up BME280 testing..."));
 
+    // Checking if the I2C connection is setup correctly 
     bool status;
-
-    // default settings
-    // (you can also pass in a Wire library object like &Wire2)
     status = bme.begin(0x77);  
     if (!status) {
-    Serial.println("Could not find a valid BME280 sensor, check wiring!");
-    while (1);
+        Serial.println("Could not find a valid BME280 sensor, check wiring!");
+        while (1);
     }
 
-    Serial.println("-- Default Test --");
-    delayTime = 1000;
-
-    Serial.println();
+    Serial.println("\n----- Starting Test -----\n");
 }
-
 
 void loop() { 
     printValues();
-    delay(delayTime);
+    delay(2000);
 }
 
 void printValues() {
