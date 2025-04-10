8b40ece47df132a37beeb23a5addee61ffad6832
Luke Watson <lukewatsontoronto@gmail.com>
2025-04-10 00:03:26 -0400

    FSW Update: Improved state flow and processor recovery

--- telemetry_source/lib/helpers/helpers.h
+++ telemetry_source/lib/helpers/helpers.h
@@ -5,10 +5,16 @@
 
 int extract_cmd_msg(const char *buff, struct command_packet *packet);
 
+const char* op_state_to_string(mission_info_struct::OperatingState state);
+
+const char* op_mode_to_string(mission_info_struct::OperatingMode mode, int full);
+
+void cmd_buff_to_echo(char *cmd_buff, char *cmd_echo);
+
 char* build_data_str(struct transmission_packet *packet);
 
 int compare_strings(const char *a, const char *b);
 
-int time_format_check(const char *string);
+int set_time_from_str(const char *time_utc);
 
 #endif /* HELPERS_H */
