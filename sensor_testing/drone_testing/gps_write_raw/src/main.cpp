8e5c3a932838715b09d45208a473d8568c7837fd
Daniel Yu <danielu6776@gmail.com>
2025-06-04 00:13:36 -0400

    GPS testing using if conditional

--- sensor_testing/drone_testing/gps_write_raw/src/main.cpp
+++ sensor_testing/drone_testing/gps_write_raw/src/main.cpp
@@ -38,11 +38,59 @@ void loop() {
     // }
 
     // Process incoming GPS data
-    while (GPS_Serial.available() > 0) {
-        char c = GPS_Serial.read();
-        //gpsFile.write(c);      // Write raw data to the file
-        Serial.write(c);       // Print raw data to Serial Monitor
-        gps.encode(c);         // Feed data to TinyGPS++ for parsing
+    // while (GPS_Serial.available() > 0) {
+    //     char c = GPS_Serial.read();
+    //     //gpsFile.write(c);      // Write raw data to the file
+    //     Serial.write(c);       // Print raw data to Serial Monitor
+    //     gps.encode(c);         // Feed data to TinyGPS++ for parsing
+    // }
+
+    // int gps_avail = GPS_Serial.available();
+    // for(int i = 0; i < gps_avail; i++)
+    // {
+    //     //Serial.print(GPS_Serial.read());
+    //     //Serial.print(GPS_Serial.readStringUntil('\n'));
+    //     Serial.print((char)GPS_Serial.read());
+    //     gps.encode(GPS_Serial.read());
+    // }
+
+    // Get the number of available bytes
+    int gps_avail = GPS_Serial.available();
+    String gpsData = "";
+    for (int i = 0; i < gps_avail; i++) {
+        gpsData += (char)GPS_Serial.read();
+    }
+
+    // Print the full burst to Serial
+    if (gpsData.length() > 0) {
+        Serial.print("RAW GPS BURST: \n");
+        Serial.print(gpsData);
+        // Feed each character to TinyGPS++
+        for (size_t i = 0; i < gpsData.length(); i++) {
+            gps.encode(gpsData[i]);
+        }
+    }
+
+    // Print parsed GPS info if available
+    if (gps.location.isValid() && gps.location.isUpdated()) {
+        Serial.print("\n\nGPS DATA:\n");
+        Serial.print("Latitude: ");
+        Serial.println(gps.location.lat(), 6);
+        Serial.print("Longitude: ");
+        Serial.println(gps.location.lng(), 6);
+        Serial.print("Altitude: ");
+        Serial.println(gps.altitude.meters());
+        Serial.print("Satellites: ");
+        Serial.println(gps.satellites.value());
+        Serial.print("Time (UTC): ");
+        if (gps.time.isValid()) {
+            char timeStr[16];
+            sprintf(timeStr, "%02d:%02d:%02d", gps.time.hour(), gps.time.minute(), gps.time.second());
+            Serial.println(timeStr);
+        } else {
+            Serial.println("INVALID");
+
+        Serial.println();
     }
 
     //gpsFile.close(); // Close the file to save after changes
